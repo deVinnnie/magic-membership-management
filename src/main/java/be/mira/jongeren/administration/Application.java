2ee298d93164c15a311c5ef2f9e2f12043c24a2c
Vincent Ceulemans <deVinnnie@users.noreply.github.com>
2017-04-17 16:07:05 +0200

    Remove unused imports

--- src/main/java/be/mira/jongeren/administration/Application.java
+++ src/main/java/be/mira/jongeren/administration/Application.java
@@ -2,11 +2,6 @@ package be.mira.jongeren.administration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.ServletContextInitializer;
-import org.springframework.context.annotation.Configuration;
-
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
 
 @SpringBootApplication
 public class Application {
