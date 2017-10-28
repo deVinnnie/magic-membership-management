0c3decc053785b06bd7e5773f36d59b5a7fe10e5
Vincent Ceulemans <deVinnnie@users.noreply.github.com>
2017-10-28 21:39:00 +0200

    Add icons for PartakingType

--- src/main/java/be/mira/jongeren/administration/domain/PartakingType.java
+++ src/main/java/be/mira/jongeren/administration/domain/PartakingType.java
@@ -1,7 +1,18 @@
 package be.mira.jongeren.administration.domain;
 
 public enum PartakingType {
-    DEELNEMER,
-    LEIDING,
-    EXTERN
+    DEELNEMER("participant"),
+    LEIDING("assistant"),
+    EXTERN("external");
+
+    private String representation;
+
+    PartakingType(String representation){
+        this.representation = representation;
+    }
+
+    @Override
+    public String toString() {
+        return this.representation;
+    }
 }
